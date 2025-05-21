0535936e4f432ac00899944af7b0e0462718b1e9
LuisKoch03 <167709716+LuisKoch03@users.noreply.github.com>
2025-05-21 16:58:04 +0200

    Suchfunktion und Kommentare

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import JumanjiBild from './Bilder/Jumanji.jpg';
-import LiloundStichBild from './Bilder/LiloundStich.jpg';
+import LiloundStitchBild from './Bilder/LiloundStitch.jpg';
 import Thunderbolts from './Bilder/Thunderbolts.jpg';
 import TheAccountTwo from './Bilder/TheAccountTwo.jpg';
 import MissionImpossible from './Bilder/MissionImpossible.jpg';
@@ -16,66 +17,112 @@ import UntilDawn from './Bilder/UntilDawn.jpg';
 import TheTransporter from './Bilder/TheTransporter.jpg';
 import StarWars from './Bilder/StarWars.png';
 
-
-
 function App() {
- const navigate = useNavigate();
+  const navigate = useNavigate();
+  
+  /* Suche */
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+  if (!searchTerm) return;
+
+  const searchTermNormalized = searchTerm.toLowerCase().replace(/\s/g, '');
+
+  const imageIds = [
+    'jumanji',
+    'liloundstitchbild',
+    'thunderbolts',
+    'theaccounttwo',
+    'missionimpossible',
+    'batman',
+    'minecraft',
+    'harrypotter',
+    'herrderringe',
+    'theamateur',
+    'fastandfurious',
+    'spongebob',
+    'untildawn',
+    'thetransporter',
+    'starwars',
+  ];
+
+  const foundId = imageIds.find(id => id.includes(searchTermNormalized));
+
+  if (foundId) {
+    const element = document.getElementById(foundId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      setShowSearch(false);
+    }
+  } else {
+    alert('Kein Film gefunden!');
+  }
+};
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+  /* Seite */
   return (
     <>
-    <div className="buttonBar">
-      <button className="suchButton">Suchen</button>
-      <button className="ticketButton"onClick={() => navigate('/Tickets')}>Tickets</button>
-      <button className="accountButton"onClick={() => navigate('/Account')}>Account</button>
-    </div>
-    <div className = "bild-galerie">
-    <a href="/Jumanji">
-      <img src={JumanjiBild} alt="Jumanji" />
-    </a>
-    <a href="/LiloundStichBild">
-      <img src={LiloundStichBild} alt="LiloundStichBild" />
-    </a>
-    <a href="/Thunderbolts">
-      <img src={Thunderbolts} alt="Thunderbolts" />
-    </a>
-    <a href="/TheAccountTwo">
-      <img src={TheAccountTwo} alt="TheAccountTwo" />
-    </a>
-    <a href="/MissionImpossible">
-      <img src={MissionImpossible} alt="MissionImpossible" />
-    </a>
-    <a href="/Batman">
-      <img src={Batman} alt="Batman" />
-    </a>
-    <a href="/Minecraft">
-      <img src={Minecraft} alt="Minecraft" />
-    </a>
-    <a href="/HarryPotter">
-      <img src={HarryPotter} alt="HarryPotter" />
-    </a>
-    <a href="/HerrDerRinge">
-      <img src={HerrDerRinge} alt="HerrDerRinge" />
-    </a>
-    <a href="/TheAmateur">
-      <img src={TheAmateur} alt="TheAmateur" />
-    </a>
-    <a href="/FastandFurious">
-      <img src={FastandFurious} alt="FastandFurious" />
-    </a>
-    <a href="/Spongebob">
-      <img src={Spongebob} alt="Spongebob" />
-    </a>
-    <a href="/UntilDawn">
-      <img src={UntilDawn} alt="UntilDawn" />
-    </a>
-    <a href="/TheTransporter">
-      <img src={TheTransporter} alt="TheTransporter" />
-    </a>
-    <a href="/StarWars">
-      <img src={StarWars} alt="StarWars" />
-    </a>
-  </div>
-  </>
+      <div className="buttonBar">
+        <button
+          className="suchButton"
+          onClick={() => setShowSearch(prev => !prev)} /* Suchleiste anzeigen */
+        >
+          Suchen
+        </button>
+        <button
+          className="ticketButton"
+          onClick={() => navigate('/Tickets')} /* Navigation zu /Tickets */
+        >
+          Tickets
+        </button>
+        <button
+          className="accountButton"
+          onClick={() => navigate('/Account')} /* Navigation zu /Account */
+        >
+          Account
+        </button>
+      </div>
 
+      {showSearch && (
+        <div className="suchbereich">
+          <input
+            type="text"
+            placeholder="Suchbegriff eingeben..."
+            autoFocus
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button onClick={handleSearch}>
+            Los
+          </button>
+        </div>
+      )}
+
+      <div className="bild-galerie">
+        <a id="jumanji" href="/Jumanji"><img src={JumanjiBild} alt="Jumanji" /></a>
+        <a id="liloundstitch" href="/LiloundStitchBild"><img src={LiloundStitchBild} alt="LiloundStitchBild" /></a>
+        <a id="thunderbolts" href="/Thunderbolts"><img src={Thunderbolts} alt="Thunderbolts" /></a>
+        <a id="theaccounttwo" href="/TheAccountTwo"><img src={TheAccountTwo} alt="TheAccountTwo" /></a>
+        <a id="missionimpossible" href="/MissionImpossible"><img src={MissionImpossible} alt="MissionImpossible" /></a>
+        <a id="batman" href="/Batman"><img src={Batman} alt="Batman" /></a>
+        <a id="minecraft" href="/Minecraft"><img src={Minecraft} alt="Minecraft" /></a>
+        <a id="harrypotter" href="/HarryPotter"><img src={HarryPotter} alt="HarryPotter" /></a>
+        <a id="herrderringe" href="/HerrDerRinge"><img src={HerrDerRinge} alt="HerrDerRinge" /></a>
+        <a id="theamateur" href="/TheAmateur"><img src={TheAmateur} alt="TheAmateur" /></a>
+        <a id="fastandfurious" href="/FastandFurious"><img src={FastandFurious} alt="FastandFurious" /></a>
+        <a id="spongebob" href="/Spongebob"><img src={Spongebob} alt="Spongebob" /></a>
+        <a id="untildawn" href="/UntilDawn"><img src={UntilDawn} alt="UntilDawn" /></a>
+        <a id="thetransporter" href="/TheTransporter"><img src={TheTransporter} alt="TheTransporter" /></a>
+        <a id="starwars" href="/StarWars"><img src={StarWars} alt="StarWars" /></a>
+      </div>
+    </>
   );
 }
 
