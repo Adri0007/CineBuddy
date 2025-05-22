98fa7cad00be7e257f94e26a229fd60c3ecb6ed4
LuisKoch03 <167709716+LuisKoch03@users.noreply.github.com>
2025-05-22 18:12:11 +0200

    Erste API, Dynamische Startseite, Neue Filmliste.json

---
+++ frontend/src/pages/Old_Startseite.jsx
@@ -0,0 +1,105 @@
+// src/pages/Startseite.jsx
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faTicket, faUser } from '@fortawesome/free-solid-svg-icons';
+import './Startseite.css';
+
+import JumanjiBild from '../Bilder/Jumanji.jpg';
+import LiloundStitchBild from '../Bilder/LiloundStitch.jpg';
+import Thunderbolts from '../Bilder/Thunderbolts.jpg';
+import TheAccountTwo from '../Bilder/TheAccountTwo.jpg';
+import MissionImpossible from '../Bilder/MissionImpossible.jpg';
+import Batman from '../Bilder/Batman.jpg';
+import Minecraft from '../Bilder/Minecraft.jpg';
+import HarryPotter from '../Bilder/HarryPotter.jpg';
+import HerrDerRinge from '../Bilder/HerrDerRinge.jpg';
+import TheAmateur from '../Bilder/TheAmateur.jpg';
+import FastandFurious from '../Bilder/FastandFurious.jpg';
+import Spongebob from '../Bilder/Spongebob.jpg';
+import UntilDawn from '../Bilder/UntilDawn.jpg';
+import TheTransporter from '../Bilder/TheTransporter.jpg';
+import StarWars from '../Bilder/StarWars.png';
+
+function Startseite() {
+  const navigate = useNavigate();
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    if (!searchTerm) return;
+    const searchTermNormalized = searchTerm.toLowerCase().replace(/\s/g, '');
+    const imageIds = [
+      'jumanji', 'liloundstitch', 'thunderbolts', 'theaccounttwo',
+      'missionimpossible', 'batman', 'minecraft', 'harrypotter',
+      'herrderringe', 'theamateur', 'fastandfurious',
+      'spongebob', 'untildawn', 'thetransporter', 'starwars',
+    ];
+    const foundId = imageIds.find(id => id.includes(searchTermNormalized));
+    if (foundId) {
+      const element = document.getElementById(foundId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+        setShowSearch(false);
+      }
+    } else {
+      alert('Kein Film gefunden!');
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') handleSearch();
+  };
+
+  return (
+    <>
+      <div className="buttonBar">
+        <button className="suchButton" onClick={() => setShowSearch(prev => !prev)}>
+          Suchen <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </button>
+        <button className="ticketButton" onClick={() => navigate('/Tickets')}>
+          Ticket <FontAwesomeIcon icon={faTicket} />
+        </button>
+        <button className="accountButton" onClick={() => navigate('/Account')}>
+          Account <FontAwesomeIcon icon={faUser} />
+        </button>
+      </div>
+
+      {showSearch && (
+        <div className="suchbereich">
+          <input
+            type="text"
+            placeholder="Suchbegriff eingeben..."
+            autoFocus
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button onClick={handleSearch}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+      )}
+
+      <div className="bild-galerie">
+        <a id="jumanji" href="/Jumanji"><img src={JumanjiBild} alt="Jumanji" /></a>
+        <a id="liloundstitch" href="/LiloundStitchBild"><img src={LiloundStitchBild} alt="LiloundStitchBild" /></a>
+        <a id="thunderbolts" href="/Thunderbolts"><img src={Thunderbolts} alt="Thunderbolts" /></a>
+        <a id="theaccounttwo" href="/TheAccountTwo"><img src={TheAccountTwo} alt="TheAccountTwo" /></a>
+        <a id="missionimpossible" href="/MissionImpossible"><img src={MissionImpossible} alt="MissionImpossible" /></a>
+        <a id="batman" href="/Batman"><img src={Batman} alt="Batman" /></a>
+        <a id="minecraft" href="/Minecraft"><img src={Minecraft} alt="Minecraft" /></a>
+        <a id="harrypotter" href="/HarryPotter"><img src={HarryPotter} alt="HarryPotter" /></a>
+        <a id="herrderringe" href="/HerrDerRinge"><img src={HerrDerRinge} alt="HerrDerRinge" /></a>
+        <a id="theamateur" href="/TheAmateur"><img src={TheAmateur} alt="TheAmateur" /></a>
+        <a id="fastandfurious" href="/FastandFurious"><img src={FastandFurious} alt="FastandFurious" /></a>
+        <a id="spongebob" href="/Spongebob"><img src={Spongebob} alt="Spongebob" /></a>
+        <a id="untildawn" href="/UntilDawn"><img src={UntilDawn} alt="UntilDawn" /></a>
+        <a id="thetransporter" href="/TheTransporter"><img src={TheTransporter} alt="TheTransporter" /></a>
+        <a id="starwars" href="/StarWars"><img src={StarWars} alt="StarWars" /></a>
+      </div>
+    </>
+  );
+}
+
+export default Startseite;
